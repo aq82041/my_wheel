c6e7c57e5d5d28860afff1c2a51368a5eb2f3cd5
anqi <18742024663@163.com>
2020-08-07 12:26:35 +0800

    添加Input组件及其样式

---
+++ src/Input.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="wrapper" :class="{'error':error}">
+        <input type="text"
+               :value="value" :disabled="disabled" :readonly="readonly">
+        <template v-if='error'>
+            <g-icon  name="error2" class="error-icon"></g-icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Input',
+
+        props:{
+            value:{
+                type: String
+            },
+            disabled:{   //是否禁用
+                type:Boolean,
+                default:false
+            },
+            readonly:{
+                type:Boolean,
+                default:false
+            },
+            error:{
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $height:32px;
+    $border-color:#999;
+    $border-color-hover:#666;
+    $border-radius:4px;
+    $font-size:12px;
+    $box-shadow-color:rgba(0,0,0,0.5);
+    $red:#F1453D;
+    .wrapper{
+        font-size:$font-size;
+        display: inline-flex;
+        align-items: center;
+        :not(:last-child){
+            margin-right:.5em;
+        }
+        > input{
+            height:$height;
+            border:1px solid $border-color;
+            border-radius: $border-radius;
+            padding:0 8px;
+            font-size:inherit;
+            &:hover{
+                border:1px solid $border-color-hover;
+            }
+            &:focus{
+                outline:none;
+                box-shadow:inset 0 1px 3px $box-shadow-color;
+            }
+            &[disabled],&[readonly]{
+                border-color:#bbb;
+                color:#bbb;
+                cursor:not-allowed;
+            }
+
+        }
+        &.error{
+            > input{
+                border-color:$red;
+            }
+        }
+        > .error-message{
+            color:$red;
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Button from './Button.vue'
 import Icon from './Icon.vue'
 import ButtonGroup from './ButtonGroup'
+import Input from "./Input";
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-input',Input)
 
 new Vue({
     el:'#app',
@@ -23,90 +25,3 @@ import spies from 'chai-spies'
 chai.use(spies)
 
 const expect = chai.expect;
-{
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:'setting'
-        }
-    })
-    vm.$mount('#test')
-    let useElement=vm.$el.querySelector('use')
-    console.log(useElement);
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:'setting',
-            loading:true
-        }
-    })
-    vm.$mount('#test')
-    let useElement=vm.$el.querySelector('use')
-    console.log(useElement);
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:'left',
-        }
-    })
-    vm.$mount(div)
-    let svgElement=vm.$el.querySelector('svg')
-    let {order}=window.getComputedStyle(svgElement)
-    expect(order).to.equal('1')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const button=new Constructor({
-        propsData:{
-            icon:'left',
-            iconPosition:'right'
-        }
-    })
-    button.$mount(div)
-    let svgElement=button.$el.querySelector('svg')
-    let {order}=window.getComputedStyle(svgElement)
-    expect(order).to.equal('2')
-    button.$el.remove()
-    button.$destroy()
-}
-// Mock
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const gbutton=new Constructor({
-        propsData:{
-            icon:'left',
-        }
-    })
-    gbutton.$mount(div)
-
-    let spy=chai.spy(function(){
-        console.log(1);
-    })
-
-    // 我希望看到这个函数被执行
-    gbutton.$on('click',spy)
-    let button=gbutton.$el
-    button.click()
-    expect(spy).to.have.been.called()
-}
