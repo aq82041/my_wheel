3f36ec0df9f4671b2b724168283b6844be0b7511
anqi <18742024663@163.com>
2020-08-08 22:09:31 +0800

    grid基本样式

---
+++ src/Col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="[`col-${span}`]"></div>
+</template>
+
+<script>
+    export default{
+        name:'Col',
+        props:{
+            span:[Number,String]
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        border:1px solid red;
+        background: grey;
+        width:50%;
+        height:100px;
+
+        $class-prefix:col-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './Button.vue'
 import Icon from './Icon.vue'
 import ButtonGroup from './ButtonGroup'
 import Input from "./Input";
+import Row from "./Row";
+import Col from "./Col";
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 
 new Vue({
     el:'#app',
