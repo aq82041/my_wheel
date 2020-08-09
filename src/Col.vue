6d052ffe50e3f7d9dd4698ffee5a25f0a2e945f3
anqi <18742024663@163.com>
2020-08-09 16:51:18 +0800

    响åº优化响应式，移动优先

--- src/Col.vue
+++ src/Col.vue
@@ -26,10 +26,6 @@
             offset:{
                 type:[Number,String]
             },
-            phone:{
-                type:Object,
-                validator
-            },
             ipad:{
                 type:Object,
                 validator
@@ -64,7 +60,6 @@
                 return [
                     span && `col-${span}` ,
                     offset && `offset-${offset}`,
-                    phone && `col-phone-${phone.span}`,
                     ipad && `col-ipad-${ipad.span}`,
                     narrowPc && `col-narrowPc-${narrowPc.span}`,
                     pc && `col-pc-${pc.span}`,
@@ -78,7 +73,6 @@
 
 <style lang="scss" scoped>
     .col{
-
         height:100px;
         $class-prefix:col-;
         @for $n from 1 through 24{
@@ -94,22 +88,7 @@
             }
         }
 
-        @media (max-width:576px){
-            $class-prefix:col-phone-;
-            @for $n from 1 through 24{
-                &.#{$class-prefix}#{$n} {
-                    width: ($n/24)*100%;
-                }
-            }
-
-            $class-prefix:offset-phone-;
-            @for $n from 1 through 24{
-                &.#{$class-prefix}#{$n} {
-                    margin-left: ($n/24)*100%;
-                }
-            }
-        }
-        @media (min-width:577px) and (max-width:768px){
+        @media (min-width:577px) {
             $class-prefix:col-ipad-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
@@ -124,22 +103,22 @@
                 }
             }
         }
-        @media (min-width:769px) and (max-width:992px){
-            $class-prefix:col-narrow-pc-;
+        @media (min-width:769px) {
+            $class-prefix:col-narrowPc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
                     width: ($n/24)*100%;
                 }
             }
 
-            $class-prefix:offset-narrow-pc-;
+            $class-prefix:offset-narrowPc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
                     margin-left: ($n/24)*100%;
                 }
             }
         }
-        @media (min-width:993px) and (max-width:1200px){
+        @media (min-width:993px) {
             $class-prefix:col-pc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
@@ -155,14 +134,14 @@
             }
         }
         @media (min-width:1201px) {
-            $class-prefix:col-wide-pc-;
+            $class-prefix:col-widePc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
                     width: ($n/24)*100%;
                 }
             }
 
-            $class-prefix:offset-wide-pc-;
+            $class-prefix:offset-widePc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n} {
                     margin-left: ($n/24)*100%;
