3a44cd3ee5e283a58accbd6b6de40dc635ddb319
anqi <18742024663@163.com>
2020-08-10 16:42:14 +0800

    只能由有一个toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,20 +1,24 @@
 import Toast from "./Toast";
 
+function createToast({Vue,message,toastOptions}){
+    let Constructor=Vue.extend(Toast)
+    let toast=new Constructor({
+        propsData:toastOptions
+    })   //创建一个toast组件
+    toast.$slots.default=message    //把信息放到插槽里
+    toast.$mount()                  //挂载
+    document.body.appendChild(toast.$el) //把toast组件放进页面
+    return toast
+}
+
+let currentToast
 export default{
     install(Vue,options){
         Vue.prototype.$toast=function(message,toastOptions){
-            let Constructor=Vue.extend(Toast)
-            let toast=new Constructor({
-                propsData:{
-                    closeButton: toastOptions.closeButton,
-                    enableHtml:toastOptions.enableHtml,
-                    autoClose: toastOptions.autoClose,
-                    position:toastOptions.position,
-                }
-            })   //创建一个toast组件
-            toast.$slots.default=message    //把信息放到插槽里
-            toast.$mount()                  //挂载
-            document.body.appendChild(toast.$el) //把toast组件放进页面
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast=createToast({Vue,message,toastOptions})
         }
     }
 }
