0b7b87a07e5a7a4247592f5d6b4a2683ab1095c7
anqi <18742024663@163.com>
2020-08-14 19:31:37 +0800

    修复

--- docs/components/toast.md
+++ docs/components/toast.md
@@ -3,7 +3,7 @@ title:Toast-消息提示
 ---
 
 # Toast-消息提示
-分隔内容上有关联但属于不同类别的数据集合。
+用于弹出消息框
 <ClientOnly>
     <toast-demo></toast-demo> 
 </ClientOnly>
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+import Vue from 'vue'
 import Button from "./src/Button";
 import ButtonGroup from "./src/ButtonGroup";
 import Icon from "./src/Icon";
+import Input from "./src/Input";
+import Row from "./src/Row";
+import Col from "./src/Col";
+import Layout from './src/Layout'
+import Sider from './src/Sider'
+import Header from './src/Header'
+import Content from './src/Content'
+import Footer from './src/Footer'
+import plugin from './src/plugin.js'
+import TabsHead from './src/Tabs-Head'
+import TabsBody from './src/Tabs-Body'
+import TabsItem from './src/Tabs-Item'
+import TabsPane from './src/Tabs-Pane'
+import Tabs from './src/Tabs'
+import Popover from "./src/Popover";
+import Collapse from "./src/Collapse";
+import CollapseItem from './src/Collapse-Item'
 
-export {Button,ButtonGroup,Icon}
+Vue.use(plugin)
+
+export {Button,ButtonGroup,Icon,Input,Row,Col,Layout,
+    Sider,Header,Content,Footer,plugin,Tabs,TabsBody,
+    TabsHead,TabsItem,TabsPane,Popover,Collapse,CollapseItem}
